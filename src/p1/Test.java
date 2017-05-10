0cad9963c81a950d1a526f928d6363fbe60ba73e
Sunny <839804356@qq.com>
2017-05-10 16:29:39 +0800

    123

--- src/p1/Test.java
+++ src/p1/Test.java
@@ -5,8 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
 		System.out.println("哈哈哈");
+		System.out.println("你螺母");
 		System.out.println("黄嘉城");
-
 	}
 
 }
